34bc2e4cc2c6d2d34b7229c3c7136fed81e8f4af
Dmitriy Chebotar <optimussflash@ukr.net>
2020-09-20 22:32:40 +0300

    delete unnecessary code

--- app/src/main/java/com/optimus/reddittop/extensions/Double.kt
+++ app/src/main/java/com/optimus/reddittop/extensions/Double.kt
@@ -35,23 +35,4 @@ enum class TimeUnits(val value: Long) {
         val result = (timeUnitDiff.toDouble() / this.value).roundToInt()
         return result.toString()
     }
-
-//    private fun plural(value: Int): String {
-//        val plurals = mapOf(
-//            SECOND to Triple("секунду", "секунды", "секунд"),
-//            MINUTE to Triple("минуту", "минуты", "минут"),
-//            HOUR to Triple("час", "часа", "часов"),
-//            DAY to Triple("день", "дня", "дней")
-//        )
-//
-//        val mark10 = value % 10
-//        val mark100 = value % 100
-//
-//        return when {
-//            mark100 in 10..20 -> "$value ${plurals[this]?.third}"
-//            mark10 == 1 -> "$value ${plurals[this]?.first}"
-//            mark10 in 2..4 -> "$value ${plurals[this]?.second}" //-
-//            else -> "$value ${plurals[this]?.third}"
-//        }
-//    }
 }
